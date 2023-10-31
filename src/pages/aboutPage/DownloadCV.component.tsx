048f478bb1f63b3b063ca9a4677c40ed9a538557
Roman_cox <rom.cox@gmail.com>
2023-11-01 00:10:40 +0300

    refactor links for download cv

--- src/pages/aboutPage/DownloadCV.component.tsx
+++ src/pages/aboutPage/DownloadCV.component.tsx
@@ -1,4 +1,5 @@
 import PictureAsPdfRoundedIcon from '@mui/icons-material/PictureAsPdfRounded';
+import CV_ELmin_eng from '../../assets/files/CV_ELmin_eng.pdf';
 
 import {Title} from '../../components/title';
 
@@ -11,11 +12,12 @@ export const DownloadCV = () => {
 			<DownloadMenuTitleContainer>
 				<Title variant={'h2'} color={'#fff'} text={'Choose CV'}/>
 			</DownloadMenuTitleContainer>
-			<CVDownloadStyled href={'/src/assets/files/CV_ELmin_eng.pdf'} download>
+			{/*<CVDownloadStyled href={'/src/assets/files/CV_ELmin_eng.pdf'} download target={'_blank'}>*/}
+			<CVDownloadStyled href={CV_ELmin_eng} download target={'_blank'}>
 				<PictureAsPdfRoundedIcon/>
                 Download English CV
 			</CVDownloadStyled>
-			<CVDownloadStyled href={'/src/assets/files/CV_ELmin_rus.pdf'} download>
+			<CVDownloadStyled href={'/src/assets/files/CV_ELmin_rus.pdf'} download target={'_blank'}>
 				<PictureAsPdfRoundedIcon/>
                 Download Russian CV
 			</CVDownloadStyled>
